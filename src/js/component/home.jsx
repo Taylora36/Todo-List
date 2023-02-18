87d5396e4d5782a46ef27ac370b8f0e727f8e92f
Taylora36 <taylor.anhalt36@gmail.com>
2023-02-18 16:35:47 +0000

    fetch-todo-list-complete

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,64 +1,69 @@
-import React, {useState} from "react";
-//import "./index.css";
-//import ReactDOM from "react-dom";
+import React, {useState, useEffect} from "react";
 //create your first component
 // const ToDos = props => {
 // 	const [tasks, setTasks] = useState(null);
-// 	useEffect(
-// 		() =>
-// 	)
-// }
-function App() {
-	const [list, setList] = useState([]);
-	const [input, setInput] = useState("");
 
-	const addTodo = (todo) => {
-		const newToDo = {
-			id: Math.random(),
-			todo: todo,
+function App() {
+	const [newItem, setNewItem] = useState("");
+	const [items, setItems] = useState([]);
+
+	function addItem() {
+		if (!newItem){
+			alert("Enter an Item.")
+			return;
+		}
+		const item = {
+			done:false,
+			id:Math.floor(Math.random()* 1000),
+			label: newItem,
 		};
 		//add the todo to the list
-		setList([...list, newToDo]);
+		setItems(oldList => [...oldList, item]);
 		//clear input box
-		setInput("");
+		setNewItem("");
 	};
-	const deleteTodo = (id) =>{
+	function deleteItem(id){
 		//filter out todo with the id
-		const newList = list.filter((todo) => todo.id !== id);
-
-		setList(newList);
+		const newArray = items.filter(item => item.id !==id);
+		setItems(newArray);
 	};
+	useEffect(async () =>{
+		const options = {
+			method: "GET",
+			headers: { "Content-Type": "application/json" },
+		  };
+		const resp = await fetch("https://assets.breatheco.de/apis/fake/todos/user/sage",options)
+		const store = await resp.json()
+		setItems(store)
+	}, [])
+	useEffect (async () =>{
+		const options = {
+			method: "PUT",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify(items),
+		  };
+	  
+		  fetch(
+			"https://assets.breatheco.de/apis/fake/todos/user/sage",
+			options
+		  ).catch((err) => console.error(err));
+	}, [items])
 	return (
 	  <div className="app">
 		<div className="container">
 		<h1>Sage's To Do List</h1>
-		 <input className="form" type="text" value={input} onChange={(e) => setInput(e.target.value)}/>
-		<button className="add" onClick={() => addTodo(input)}>Add</button>
+		 <input
+		 className="form"
+		 placeholder="Add an item..."
+		 type="text" value={newItem}
+		 onChange={e => setNewItem(e.target.value)}/>
+		<button className="add" onClick={() => addItem()}>Add</button>
 		<ul className="list-group">
-			<li className="list-group-item d-flex justify-content-between align-items-center">
-				<span>Squeek</span>
-				<button className="hovertext" data-hover="&times;" onClick={() => deleteTodo(todo.id)}>&times;</button>
-			</li>
-			<li className="list-group-item d-flex justify-content-between align-items-center">
-				<span>Beg for salmon</span>
-				<button className="delete" onClick={() => deleteTodo(todo.id)}>&times;</button>
-			</li>
-			<li className="list-group-item d-flex justify-content-between align-items-center">
-				<span>Watch TV (preferrably a violent show)</span>
-				<button className="delete" onClick={() => deleteTodo(todo.id)}>&times;</button>
-			</li>
-			<li className="list-group-item d-flex justify-content-between align-items-center">
-				<span>Cuddle with human</span>
-				<button className="delete" onClick={() => deleteTodo(todo.id)}>&times;</button>
-			</li>
-		</ul>
-		<ul className="list-group">
-			{list.map((todo) => (
-				<li className="list-group-item d-flex justify-content-between align-items-center" key={todo.id}>
-					{todo.todo}
-					<button className="delete" onClick={() => deleteTodo(todo.id)}>&times;</button>
-				</li>
-			))}
+			{items.map(item =>{
+				return(
+					<li key={item.id}>{item.label} <button className="delete-button" onClick={() => deleteItem(item.id)}>x</button></li>
+				)
+			})}
 		</ul> 
 		</div>
 	  </div>
